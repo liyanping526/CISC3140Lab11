27387b1ccc5c30e00a97f0a91e783aeda002d929
liyanping526 <31420754+liyanping526@users.noreply.github.com>
2020-11-22 22:11:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction (){
+	var x = document.getElementById("text");
+	if(x.innerHTML ==="Hi"){
+		x.innerHTML ="Welcome to Food Paradise";
+	}else{
+		x.innerHTML="Hi";
+	}
+}
+function changeColor(){
+	document.body.style.backgroundColor = 'yellow';
+}
